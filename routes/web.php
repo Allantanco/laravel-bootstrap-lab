0b07ed398a6df7c3e99dee0c48faffe8bd58fb48
Allan Tanco Jr <https://github.com/Allantanco/laravel-bootstrap-lab.git>
2026-09-07 16:34:44 +0800

    Add PageController and new views, update login and routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
-<?php
+<<?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route pointing to the index method
+Route::get('/', [PageController::class, 'index']);
+
+// Route pointing to the about method
+Route::get('/about', [PageController::class, 'about']);
